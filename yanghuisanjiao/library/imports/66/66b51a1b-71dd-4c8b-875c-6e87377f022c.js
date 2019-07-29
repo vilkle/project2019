65bbbbbce5866554b145cb2e0c404fe8013a67a2
vilkle <525053561@qq.com>
2019-07-29 14:54:23 +0800

    update

--- yanghuisanjiao/library/imports/66/66b51a1b-71dd-4c8b-875c-6e87377f022c.js
+++ yanghuisanjiao/library/imports/66/66b51a1b-71dd-4c8b-875c-6e87377f022c.js
@@ -7,7 +7,7 @@ var ConstValue = /** @class */ (function () {
     function ConstValue() {
     }
     ConstValue.IS_EDITIONS = true; //是否为发布版本，用于数据上报 及 log输出控制
-    ConstValue.IS_TEACHER = false; //是否为教师端版本
+    ConstValue.IS_TEACHER = true; //是否为教师端版本
     ConstValue.CONFIG_FILE_DIR = "config/";
     ConstValue.PREFAB_UI_DIR = "prefab/ui/panel/";
     ConstValue.AUDIO_DIR = "audio/";
