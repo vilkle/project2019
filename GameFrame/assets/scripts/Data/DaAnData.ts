d7684894b3d32142754089edb316df0f0641f961
vilkle <525053561@qq.com>
2019-11-22 16:49:59 +0800

    ypdate

---
+++ GameFrame/assets/scripts/Data/DaAnData.ts
@@ -0,0 +1,13 @@
+export class DaAnData {
+    private static instance: DaAnData;
+    
+    
+    public submitEnable = false;
+   
+    static getInstance() {
+        if (this.instance == null) {
+            this.instance = new DaAnData();
+        }
+        return this.instance;
+    }
+}
